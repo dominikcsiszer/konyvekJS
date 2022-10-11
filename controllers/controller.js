a75123e410a03ef33fded86487a78a55a44379e7
dominikcsiszer <dominikcsiszer@icloud.com>
2022-10-11 11:44:56 +0200

    View és model kész

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,6 +1,16 @@
+import Model from "../models/model.js"
+import View from "../views/View.js"
+
 class Controller {
+    
     constructor() {
-        
+        let model = new Model()
+        model.fetchData('../adat.json', this.konyvData)
+    }
+
+    konyvData(data) {
+        let adminView = new View(data, 'tbody', true)
+        let normaliew = new View(data, '.konyvek')
     }
 }
 
--- models/model.js
+++ models/model.js
@@ -1,6 +1,25 @@
 class Model {
+
+    #adat = []
     constructor() {
-        
+
+    }
+
+    fetchData(vegpont, myCallback) {
+        fetch(vegpont, {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+        }
+        })
+        .then((response) => response.json())
+        .then((data) => {
+            this.#adat = data.konyvek
+            myCallback(this.#adat)
+        })
+        .catch((error) => {
+            console.error("Error:", error)
+        })
     }
 }
 
